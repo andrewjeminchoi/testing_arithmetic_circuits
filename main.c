22902d05c8936ce699b729d5167532bab14661a7
Andrew Choi <andrewchoi96@hotmail.com>
2017-07-28 18:48:50 -0700

    Added backprop

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /* 
  * File:   main.c
- * Author: andre
+ * Author: andrewchoi
  *
  * Created on July 25, 2017, 10:30 AM
  */
@@ -9,9 +9,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-/*
- * 
+/* Reads an .ac file by argument and calculates the circuit output and 
+ * partial derivatives for every node.
+ * The circuit is stored in an adjacency list.
+ * Each non-leaf node storess its children in a separate array.
  */
+
+/* Node Structure
+   Children and flag only apply to non-leaf nodes*/
 struct node {
   /*Node type can be 'n' or 'v' for leaf nodes
     Node type can be '+' or '-' for non-leaf nodes */
@@ -23,12 +28,14 @@ struct node {
   /*Derivative of the node*/
   double dr; 
   /*Children nodes*/
+  /*Currently assumes a binary AC (only two children per node)*/
   //struct node **child;
   int child[2];
   /*Bit flag, true means there is exactly one child that is zero*/
   bool flag;
   /*Zero counter, if this counter is 1, set flag to true*/
-  int counter;
+  /*Currently not used (assuming binary AC structure)*/
+  //int counter;
 };
 
 
@@ -37,10 +44,9 @@ int main(int argc, char** argv) {
   FILE *ac_file;
   char lineToRead[5000]; 
   struct node **circuit;
-  struct node* n;
+  struct node *n;
   int index = 0;
   
-    
   /*Try to open the AC file*/
   if (argc < 2) {
     /*No file has been passed - error*/
@@ -57,14 +63,18 @@ int main(int argc, char** argv) {
   }
     
   /*File was successfully read*/
-  /*Allocate memory for the circuit*/
-  circuit = (struct node**)malloc(sizeof(struct node*) * 50);
   while (fgets(lineToRead, 5000, ac_file) != NULL) {
-    printf("%s", lineToRead);
-    
+    //printf("%s", lineToRead);
         
     if (*lineToRead == '(') {
-      /*Do nothing*/
+      printf("\t... reading file ...\n");
+      /*Allocate memory for the circuit*/
+      circuit = (struct node**)malloc(sizeof(struct node*) * 1000);
+    }
+    else if (*lineToRead == 'E'){
+      printf("\t... done reading file ... \n");
+      index--;
+      n->dr = 1;
     }
     else{
       if (*lineToRead == 'n') {
@@ -72,15 +82,15 @@ int main(int argc, char** argv) {
 	/*Insert node into circuit*/
 	n = (struct node*)malloc(sizeof(struct node));
 	sscanf(lineToRead, "%s %lf", &(n->nodeType), &(n->vr));
+	n->dr = 0;
 	n->flag = false;
-	//printf("in: %c, val: %lf \n", n->nodeType, n->vr);
       }
       else if (*lineToRead == 'v') {
 	/*Leaf node (Variable)*/
 	n = (struct node*)malloc(sizeof(struct node));
 	sscanf(lineToRead, "%s %d %lf", &(n->nodeType), &(n->index), &(n->vr));
+	n->dr = 0;
 	n->flag = false;
-	//printf("in: %c %d, val: %lf \n", n->nodeType, n->index, n->vr);
       }
       else if (*lineToRead == '+') {
 	/*Non-leaf (Operation)*/
@@ -89,6 +99,7 @@ int main(int argc, char** argv) {
 	sscanf(lineToRead, "%s %d %d", &(n->nodeType), &(n->child[0]), &(n->child[1]));
 	n->flag = false;
 	n->vr = 0;
+	n->dr = 0;
 	
 	/*Only add values if the flag is down*/
 	if (!circuit[n->child[0]]->flag) {
@@ -97,14 +108,15 @@ int main(int argc, char** argv) {
 	if (!circuit[n->child[1]]->flag) {
 	  n->vr += circuit[n->child[1]]->vr;
 	}
+	/*Incorrect output when using bit flags*/
 	//n->vr = circuit[n->child[0]]->vr + circuit[n->child[1]]->vr;
-	//printf("in: %c %d, val: %lf \n", n->nodeType, n->index, n->vr);
       }
       else if (*lineToRead == '*') {
 	/*Non-leaf (Operation)*/
 	n = (struct node*)malloc(sizeof(struct node));
 	sscanf(lineToRead, "%s %d %d", &(n->nodeType), &(n->child[0]), &(n->child[1]));
         n->vr = 1;
+	n->dr = 0;
 
 	/*Raise bit flag if there is exactly one child with value equal to 0*/
 	if (circuit[n->child[0]]->vr == 0 && circuit[n->child[1]]->vr != 0) {
@@ -142,39 +154,68 @@ int main(int argc, char** argv) {
 	    n->vr = 0;
 	  }
 	}
-	
-	//printf("in: %c %d, val: %lf \n", n->nodeType, n->index, n->vr);
       }
-      printf("node type: %c, vr: %lf, index: %d, flag %d\n", n->nodeType, n->vr, index, n->flag);
-      //printf("node added %d\n", index);
-      n->dr = 0;
+      //printf("node type: %c, vr: %lf, index: %d, flag %d\n", n->nodeType, n->vr, index, n->flag);
       circuit[index] = n;
-      index++;
-    }
-    
-    if (*lineToRead == 'E') {
-      printf("... finished reading AC file ... \n");
-      index--;
+      index++;   
     }
   }
 
   /*Print out circuit output*/
-  printf("output %lf\n", circuit[index]->vr);
+  printf("output %lf\n\n", circuit[index]->vr);
 
   /*Bit-encoded backpropagation*/
-  
+  printf("\t... starting backpropagation ...\n");
+  struct node* parent;
+  for (int i = index; i >= 0; i--) {
+    parent = circuit[i];
+
+    /*Assign dr values depending on parent node*/
+    if (parent->nodeType == '+') {
+      circuit[parent->child[0]]->dr = parent->dr;
+      circuit[parent->child[1]]->dr = parent->dr;
+    }
+    else if (parent->nodeType == '*') {
+      /*if bit flag is down, and parent is non-zero, dr(c) = dr(p)*vr(p)/vr(c)*/
+      if (parent->dr == 0) {
+	/*Set all child nodes dr to zero*/
+	circuit[parent->child[0]]->dr = 0;
+	circuit[parent->child[1]]->dr = 0;
+      }
+      else if (parent->flag) {
+	/*Check value of all child nodes*/
+	/*if flag is up and child is zero, then dr(c) = dr(p) * vr(p)*/
+	if (circuit[parent->child[0]]->vr == 0) {
+	  circuit[parent->child[0]]->dr = parent->dr * parent->vr;
+	  /*Set all other children dr to zero*/
+	  circuit[parent->child[1]]->dr = 0;
+	}
+	else {
+	  circuit[parent->child[1]]->dr = 0;
+	  circuit[parent->child[0]]->dr = parent->dr *
+	    (parent->vr / circuit[parent->child[0]]->vr);
+	}
+      }
+      else {
+	circuit[parent->child[1]]->dr = parent->dr *
+	  (parent->vr / circuit[parent->child[1]]->vr);
+	circuit[parent->child[0]]->dr = parent->dr *
+	  (parent->vr / circuit[parent->child[0]]->vr);
+      }
+    }
+  }
   
   /*Free all nodes and circuit*/
-  for (int i = 0; i < index; i++) {
-    //printf("%d\n", i);	    
+  for (int i = 0; i <= index; i++) {
+    printf("n%d t: %c, dr: %lf vr: %lf\n", i, circuit[i]->nodeType, circuit[i]->dr, circuit[i]->vr);
     free(circuit[i]);
   }
     
   free(circuit);
-
+      
   /*Close file*/
   fclose(ac_file);
     
   return (EXIT_SUCCESS);
 }
-
+  
