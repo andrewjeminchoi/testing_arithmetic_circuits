82963acc5e21d6b069cf7fb1238da1f3c4d29270
Andrew Choi <andrewchoi96@hotmail.com>
2017-07-31 10:43:04 -0700

    Put code into functions
    Still seg faults for voting.ac

--- main.c
+++ main.c
@@ -16,7 +16,7 @@
  */
 
 /* Node Structure
-   Children and flag only apply to non-leaf nodes*/
+   Children and flag only apply to non-leaf nodes */
 struct node {
   /*Node type can be 'n' or 'v' for leaf nodes
     Node type can be '+' or '-' for non-leaf nodes */
@@ -38,20 +38,85 @@ struct node {
   //int counter;
 };
 
+struct node **circuit;
 
+/* 
+ * Function to perform bit-encoding backpropagation 
+ */
+void bit_backpropagation(int index){
+  struct node* parent;
+  for (int i = index; i >= 0; i--) {
+    parent = circuit[i];
+
+    /*Assign dr values depending on parent node*/
+    if (parent->nodeType == '+') {
+      circuit[parent->child[0]]->dr = parent->dr;
+      circuit[parent->child[1]]->dr = parent->dr;
+    }
+    else if (parent->nodeType == '*') {
+      /*if bit flag is down, and parent is non-zero, dr(c) = dr(p)*vr(p)/vr(c)*/
+      if (parent->dr == 0) {
+	/*Set all child nodes dr to zero*/
+	circuit[parent->child[0]]->dr = 0;
+	circuit[parent->child[1]]->dr = 0;
+      }
+      else if (parent->flag) {
+	/*Check value of all child nodes*/
+	/*if flag is up and child is zero, then dr(c) = dr(p) * vr(p)*/
+	if (circuit[parent->child[0]]->vr == 0) {
+	  circuit[parent->child[0]]->dr = parent->dr * parent->vr;
+	  /*Set all other children dr to zero*/
+	  circuit[parent->child[1]]->dr = 0;
+	}
+	else {
+	  circuit[parent->child[1]]->dr = 0;
+	  circuit[parent->child[0]]->dr = parent->dr *
+	    (parent->vr / circuit[parent->child[0]]->vr);
+	}
+      }
+      else {
+	circuit[parent->child[1]]->dr = parent->dr *
+	  (parent->vr / circuit[parent->child[1]]->vr);
+	circuit[parent->child[0]]->dr = parent->dr *
+	  (parent->vr / circuit[parent->child[0]]->vr);
+      }
+    }
+  }
+}
+/*
+ * Function to free all nodes and print the backpropagated values in AC
+ */
+int free_nodes(int index) {
+  if (circuit == NULL) {
+    printf("Circuit is empty!\n");
+    return (EXIT_FAILURE);
+  }
+  for (int i = 0; i <= index; i++) {
+    if (circuit[i] != NULL) {
+      printf("n%d t: %c, dr: %lf vr: %lf\n", i, circuit[i]->nodeType, circuit[i]->dr, circuit[i]->vr);
+      free(circuit[i]);
+    }
+  }
+  return (EXIT_SUCCESS);
+}
 
 int main(int argc, char** argv) {
   FILE *ac_file;
   char lineToRead[5000]; 
-  struct node **circuit;
+  //struct node **circuit;
   struct node *n;
   int index = 0;
+  int size = 0;
   
   /*Try to open the AC file*/
   if (argc < 2) {
     /*No file has been passed - error*/
     fprintf(stderr, "Must pass AC file\n");
     return(EXIT_FAILURE);
+  }
+
+  if (argc == 3) {
+    size = atoi(argv[2]);
   }
     
   ac_file = fopen(argv[1], "r");
@@ -63,18 +128,26 @@ int main(int argc, char** argv) {
   }
     
   /*File was successfully read*/
-  while (fgets(lineToRead, 5000, ac_file) != NULL) {
-    //printf("%s", lineToRead);
+  while (fgets(lineToRead, 20000, ac_file) != NULL) {
+    //printf("index: %d, %s", index, lineToRead);
         
     if (*lineToRead == '(') {
       printf("\t... reading file ...\n");
+
       /*Allocate memory for the circuit*/
-      circuit = (struct node**)malloc(sizeof(struct node*) * 1000);
+      if (size > 0) {
+	size += 20;
+	circuit = (struct node**)malloc(sizeof(struct node*) * size);
+      }
+      else {
+	circuit = (struct node**)malloc(sizeof(struct node*) * 50000);
+      }
     }
     else if (*lineToRead == 'E'){
       printf("\t... done reading file ... \n");
       index--;
       n->dr = 1;
+      break;
     }
     else{
       if (*lineToRead == 'n') {
@@ -162,60 +235,21 @@ int main(int argc, char** argv) {
   }
 
   /*Print out circuit output*/
-  printf("output %lf\n\n", circuit[index]->vr);
+  printf("output %lf for %d nodes\n\n", circuit[index]->vr, index);
 
   /*Bit-encoded backpropagation*/
   printf("\t... starting backpropagation ...\n");
-  struct node* parent;
-  for (int i = index; i >= 0; i--) {
-    parent = circuit[i];
-
-    /*Assign dr values depending on parent node*/
-    if (parent->nodeType == '+') {
-      circuit[parent->child[0]]->dr = parent->dr;
-      circuit[parent->child[1]]->dr = parent->dr;
-    }
-    else if (parent->nodeType == '*') {
-      /*if bit flag is down, and parent is non-zero, dr(c) = dr(p)*vr(p)/vr(c)*/
-      if (parent->dr == 0) {
-	/*Set all child nodes dr to zero*/
-	circuit[parent->child[0]]->dr = 0;
-	circuit[parent->child[1]]->dr = 0;
-      }
-      else if (parent->flag) {
-	/*Check value of all child nodes*/
-	/*if flag is up and child is zero, then dr(c) = dr(p) * vr(p)*/
-	if (circuit[parent->child[0]]->vr == 0) {
-	  circuit[parent->child[0]]->dr = parent->dr * parent->vr;
-	  /*Set all other children dr to zero*/
-	  circuit[parent->child[1]]->dr = 0;
-	}
-	else {
-	  circuit[parent->child[1]]->dr = 0;
-	  circuit[parent->child[0]]->dr = parent->dr *
-	    (parent->vr / circuit[parent->child[0]]->vr);
-	}
-      }
-      else {
-	circuit[parent->child[1]]->dr = parent->dr *
-	  (parent->vr / circuit[parent->child[1]]->vr);
-	circuit[parent->child[0]]->dr = parent->dr *
-	  (parent->vr / circuit[parent->child[0]]->vr);
-      }
-    }
-  }
+  bit_backpropagation(index);
   
   /*Free all nodes and circuit*/
-  for (int i = 0; i <= index; i++) {
-    printf("n%d t: %c, dr: %lf vr: %lf\n", i, circuit[i]->nodeType, circuit[i]->dr, circuit[i]->vr);
-    free(circuit[i]);
-  }
-    
+  free_nodes(index);
   free(circuit);
-      
+  
   /*Close file*/
-  fclose(ac_file);
-    
+  if (ac_file != NULL) {
+    fclose(ac_file);
+  }
+  
   return (EXIT_SUCCESS);
 }
   
